415745cbd6adfd4b3ffe1baa606cf15cbfc6af32
ajifly723 <ajifly723@gmail.com>
2024-06-16 12:15:47 +0900

    add
    blogをapiで引っ張ってくるようにhome更新(途中)

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-function Profile() {
+function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://127.0.0.1:8000/api/blog/');
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div>
-      <h1>home</h1>
-      <p>ここはホーム</p>
+      <ul>
+        {data.map(item => (
+          <li key={item.title}>{item.title}　{item.content}</li>
+        ))}
+      </ul>
     </div>
   );
 }
 
-export default Profile;
+export default App;
