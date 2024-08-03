665233ffe568e66fc5ee73d5349883bf5c6abd42
ajifly723 <ajifly723@gmail.com>
2024-08-03 23:42:42 +0900

    Update Home.jsx

--- src/components/blog/Home.jsx
+++ src/components/blog/Home.jsx
@@ -1,185 +1,186 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route, Switch, useLocation, useHistory } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import SidebarContent from './HomeSidebar';
 
-const apiUrl = process.env.REACT_APP_API_URL;
-const baseUrl = process.env.REACT_APP_BASE_URL;
+// カスタムフック: ウィンドウサイズが `576px` 以下かどうかをチェック
+const useIsSmallScreen = () => {
+    const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth <= 576);
 
+    useEffect(() => {
+        const handleResize = () => {
+            setIsSmallScreen(window.innerWidth <= 576);
+        };
 
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    return isSmallScreen;
+};
 
 // 日付表記yyyy年mm月へ
-function formatDateToJapanese(dateString){
-		const date = new Date(dateString);
-		const year = date.getFullYear();
-		const month = date.getMonth() + 1; // getMonth() は0から11までの値を返すため、+1する必要があります
-		const day = date.getDate();
-	
-		return `${year}年${month}月${day}日`;
-}
+const formatDateToJapanese = (dateString) => {
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
 
-function truncateTo100Chars(value) {
-        // HTMLタグを削除
-        const clean = /<.*?>/g;
-        value = value.replace(clean, '');
-    
-        // エスケープ文字を削除
-        value = value.replace(/&[A-Za-z0-9#]+;/g, '');
-    
-        // 100文字まで
-        if (value.length > 100) {
-            return value.slice(0, 100) + '...';
-        }
-        return value;
-}
+    return `${year}年${month}月${day}日`;
+};
+
+// テキストを100文字までトリム
+const truncateTo100Chars = (value) => {
+    const clean = /<.*?>/g;
+    value = value.replace(clean, '').replace(/&[A-Za-z0-9#]+;/g, '');
+
+    return value.length > 100 ? `${value.slice(0, 100)}...` : value;
+};
 
 // カスタムフック: URLクエリパラメータを取得
-function useQuery() {
-	return new URLSearchParams(useLocation().search);
-}
+const useQuery = () => new URLSearchParams(window.location.search);
+
+// ページネーションリンクの作成
+const Pagination = ({ currentPage, pageCount, addUrl }) => (
+    pageCount > 1 && (
+        <div className="text-center">
+            {currentPage > 1 ? (
+                <span className="ml-2"><Link to={`/?page=${currentPage - 1}${addUrl}`}>prev</Link></span>
+            ) : (
+                <span className="ml-2">prev</span>
+            )}
+
+            {Array.from({ length: pageCount }).map((_, i) => (
+                <span className="ml-2" key={i}>
+                    {currentPage !== i + 1 ? (
+                        <Link to={`/?page=${i + 1}${addUrl}`}>{i + 1}</Link>
+                    ) : (
+                        <span>{i + 1}</span>
+                    )}
+                </span>
+            ))}
+
+            {currentPage < pageCount ? (
+                <span className="ml-2"><Link to={`/?page=${currentPage + 1}${addUrl}`}>next</Link></span>
+            ) : (
+                <span className="ml-2">next</span>
+            )}
+        </div>
+    )
+);
+
+// ブログアイテムの表示
+const BlogItem = ({ item,isSmallScreen }) => (
+    <div className="col-md-6">
+        <div className="d-flex flex-column bd-highlight mb-5">
+            <div>
+                <Link className="no-link-style" to={`/detail/${item.id}`}>
+                    <div>
+                        <span className="ml-2 text-secondary mark small">
+                            <Link to={`/?category=${item.category.id}`}>{item.category.name}</Link>
+                        </span>
+
+                        {item.tag.map(tag => (
+                            <span className="ml-2 text-secondary small" key={tag.id}>
+                                <Link to={`/?tag=${tag.id}`}>{tag.name}</Link>
+                            </span>
+                        ))}
+                    </div>
+
+                    <div className="text-center">
+                        <img src={item.img} height="150" alt={item.title} />
+                    </div>
+
+                    <div>
+                        <span>
+                            <img
+                                className="mr-2 align-baseline"
+                                src={`${process.env.REACT_APP_BASE_URL}/media/icon/calendar.svg`}
+                                width="16"
+                                height="16"
+                                alt="Calendar"
+                            />
+                        </span>
+                        <span className="text-secondary align-text-bottom">
+                            {formatDateToJapanese(item.created_at)}
+                        </span>
+                    </div>
+
+                    <div>
+                        <h5>
+                            <p className="ml-1 mr-1 mt-3 mb-3">
+                                <b>{item.title}</b>
+                            </p>
+                        </h5>
+                    </div>
+
+                    <div>
+                        <span className="ml-1 mr-1 text-secondary">
+                            {truncateTo100Chars(item.content)}
+                        </span>
+                        <div className="text-right">
+                            <span>記事を読む</span>
+                        </div>
+                    </div>
+                </Link>
+				{isSmallScreen && <hr />}
+            </div>
+        </div>
+    </div>
+);
 
 const App = () => {
     const [blog, setBlog] = useState(null);
-	const [pageCount, setPageCount] = useState(null);
+    const [pageCount, setPageCount] = useState(null);
     const [currentPage, setCurrentPage] = useState(null);
 
-    // URLパラメータからフィルタ状態を取得
-	const query = useQuery();
-	const selectedPage = parseInt(query.get('page') || 1,10)
-	const selectedCategory = query.get('category') || '';
-	const selectedTag = query.get('tag') || '';
-	const selectedYearMonth = query.get('date') || '';
+    const isSmallScreen = useIsSmallScreen();
 
-    
-    const fetchItems = async (currentPage) => {
-        const response = await fetch(`${apiUrl}/blog/?page=${selectedPage}&category=${selectedCategory}&tag=${selectedTag}&date=${selectedYearMonth}`);
+    // URLパラメータからフィルタ状態を取得
+    const query = useQuery();
+    const selectedPage = parseInt(query.get('page') || 1, 10);
+    const selectedCategory = query.get('category') || '';
+    const selectedTag = query.get('tag') || '';
+    const selectedYearMonth = query.get('date') || '';
+
+    const fetchItems = async () => {
+        const response = await fetch(
+            `${process.env.REACT_APP_API_URL}/blog/?page=${selectedPage}&category=${selectedCategory}&tag=${selectedTag}&date=${selectedYearMonth}`
+        );
         const data = await response.json();
         setBlog(data.results);
-		setCurrentPage(selectedPage)
-		setPageCount(Math.ceil(data.count / 6))
+        setCurrentPage(selectedPage);
+        setPageCount(Math.ceil(data.count / 6));
     };
 
     useEffect(() => {
-        fetchItems(currentPage);
+        fetchItems();
     }, [selectedPage, selectedCategory, selectedTag, selectedYearMonth]);
 
-
     if (!blog) {
-		return <div>Loading...</div>;
-	}
+        return <div>Loading...</div>;
+    }
 
-	//ページネーション用add URL作成
-	let addUrl = []
-	if (selectedCategory) {
-        addUrl += `&category=${selectedCategory}`
-	}
-	if (selectedTag) {
-        addUrl += `&tag=${selectedTag}`
-	}
-	if (selectedYearMonth) {
-        addUrl += `&date=${selectedYearMonth}`
-	}
+    let addUrl = `${selectedCategory ? `&category=${selectedCategory}` : ''}${selectedTag ? `&tag=${selectedTag}` : ''}${selectedYearMonth ? `&date=${selectedYearMonth}` : ''}`;
 
     return (
-		<>
-		<div className="col-sm-9">
-		<div className="container container-m">
-			<div className="row">
-			{blog.map(item => (
-				<div className="col-md-6" key={item.id}>
-					<div className="d-flex flex-column bd-highlight mb-5">
-						<div>
-							<Link className="no-link-style" to={`/detail/${item.id}`}>
-						
-							<div>
-								<span className="ml-2 text-secondary mark small">
-									<Link to={`/?category=${item.category.id}`}>{item.category.name}</Link>
-								</span>
-
-								{item.tag.map(
-									tag => (
-										<span className="ml-2 text-secondary small" key={tag.id}>
-											<Link to={`/?tag=${tag.id}`}>{tag.name}</Link>
-										</span>
-								))}
-							</div>
+        <>
+            <div className="col-sm-9">
+                <div className="container container-m">
+                    <div className="row">
+                        {blog.map(item => (
+                            <BlogItem key={item.id} item={item} isSmallScreen={isSmallScreen} />
+                        ))}
+                    </div>
 
 
-							<div className="text-center">
-								<img src={item.img} height="150"/>
-							</div>
 
-							<div>
-								<span>
-									<img className="mr-2 align-baseline" src={`${baseUrl}/media/icon/calendar.svg`} width="16" height="16"/>
-								</span>
-
-								<span className="text-secondary align-text-bottom">
-									{formatDateToJapanese(item.created_at)}
-								</span>
-							</div>
-
-							<div>
-								<h5>
-									<p className="ml-1 mr-1 mt-3 mb-3">
-										<b>{item.title}</b>
-									</p>
-								</h5>
-							</div>
-
-							<div>
-								<span className="ml-1 mr-1 text-secondary">
-									{truncateTo100Chars(item.content)}
-								</span>
-								<div className="text-right">
-								<span>
-								記事を読む
-								</span>
-								</div>
-							</div>
-							
-							
-								
-									
-								
-							
-							
-							</Link>
-						</div>
-					</div>
-				</div>	
-			))}	
-			</div>
-
-			{pageCount > 1 ?
-				<div className="text-center">
-				
-				{currentPage > 1 ?
-				<span className="ml-2"><Link to={`/?page=${currentPage - 1}${addUrl}`}>prev</Link></span>
-				:
-				<span className="ml-2">prev</span>
-				}
-
-				{Array.from({ length: pageCount }).map((_, i) => (
-					currentPage != i+1 ?
-					<span className="ml-2" key={i}><Link to={`/?page=${i + 1}${addUrl}`}>{i + 1}</Link></span>
-					:
-					<span className="ml-2" key={i}>{i + 1}</span>
-				))}
-
-				{currentPage < pageCount ? 
-				<span className="ml-2"><Link to={`/?page=${currentPage + 1}${addUrl}`}>next</Link></span>
-				:
-				<span className="ml-2">next</span>
-				}
-			
-				</div>
-			:""}
-			
-			</div>
-			</div>
-			<SidebarContent />
-	</>
+                    <Pagination currentPage={currentPage} pageCount={pageCount} addUrl={addUrl} />
+                </div>
+            </div>
+            <SidebarContent />
+        </>
     );
 };
 
