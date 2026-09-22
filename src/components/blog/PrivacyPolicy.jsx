8048e9c7c199dbd5f4ff1797e34f1ebe10b05661
sanmamama <144568399+sanmamama@users.noreply.github.com>
2026-09-22 22:42:42 +0900

    最終更新日削除

--- src/components/blog/PrivacyPolicy.jsx
+++ src/components/blog/PrivacyPolicy.jsx
@@ -7,7 +7,6 @@ function PrivacyPolicy() {
 		<div className="col-sm-9">
               <div className="container container-m">
 
-		<p className="mt-0 mb-0 text-secondary">最終更新日：2024年10月11日</p>
 
 				<h3><b>プライバシーポリシー</b></h3>
 
